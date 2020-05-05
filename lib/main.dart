a8caae1d89da131f9bc17c58b0301279a3a875cf
Sudhan Pandey <pandeysudhan@sudhan.local>
2020-05-05 11:51:08 +0545

    just to chexk

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,8 +31,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
-  
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -39,10 +40,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Sudhan")),
-     
-      
+    return WebviewScaffold(
+      url: "https://www.google.com/",
+      appBar: AppBar(title: Text("Downloader")),
     );
   }
 }
